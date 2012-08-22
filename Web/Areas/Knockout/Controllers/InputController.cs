750dcf94e0d2effd0320c8ad58e6deb50dfd3f47
eiu165 <eiu165@gmail.com>
2012-08-22 00:24:30 -0500

    worked on js

--- Web/Areas/Knockout/Controllers/InputController.cs
+++ Web/Areas/Knockout/Controllers/InputController.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading;
 
 namespace Web.Areas.Knockout.Controllers
 {
+    public class InputViewModel
+    {
+        public string qty { get; set; }
+    } 
+    public class ResultViewModel
+    {
+        public int year { get; set; }
+        public float day { get; set; }
+        public float week { get; set; }
+    }
+
     public class InputController : Controller
     {
         //
@@ -16,7 +28,23 @@ namespace Web.Areas.Knockout.Controllers
             return View();
         }
 
-         
+
+        public JsonResult Calc(InputViewModel input)
+        {
+            Thread.Sleep(300); 
+
+            return Json(this.GetResults(), JsonRequestBehavior.AllowGet); //Content(r);
+        }
+
+          
+        private IQueryable<ResultViewModel>  GetResults()
+        {
+            var l = new List<ResultViewModel>();
+            l.Add(new ResultViewModel { day = 2, week = 3, year = 3 });
+            //l.Add(new TaskViewModel { title = "Wire the money to Panama", isDone = false }); 
+            return l.Where(x => true).AsQueryable(); 
+        }
+
 
 
 
--- Web/Areas/Knockout/Scripts/InputPartial.js
+++ Web/Areas/Knockout/Scripts/InputPartial.js
@@ -1,26 +1,28 @@
 ﻿
+$(function () {
+    function InputViewModel() {
+        var self = this;
+        this.qty = ko.observable("aaaa");
 
 
+        // Operations
+        self.calc = function () {
+            console.log('calc');
+            var input = new InputViewModel({ qty: self.qty._latestValue });
+            $.ajax("/Knockout/Input/Calc", {
+                data: ko.toJSON(  input ),
+                type: "post", contentType: "application/json",
+                success: function (allData) {
+                    console.log('success');
+                },
+                error: function () { alert('error'); },
+                complete: function () {
+                }
+            });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        };
+    }
+    // Activates knockout.js
+    ko.applyBindings(new InputViewModel(), $('#inputForm')[0]);
+});
 
