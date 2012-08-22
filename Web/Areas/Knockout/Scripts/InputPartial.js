296f567c3f96b7c3ae1acc1f2b0b8d7d1bb78bb5
eiu165 <eiu165@gmail.com>
2012-08-22 07:41:54 -0500

    sweet, just learned alot about knockout use ko.toJSON(self)

--- Web/Areas/Knockout/Scripts/InputPartial.js
+++ Web/Areas/Knockout/Scripts/InputPartial.js
@@ -2,15 +2,13 @@
 $(function () {
     function InputViewModel() {
         var self = this;
-        this.qty = ko.observable("aaaa");
-
-
+        self.qty = ko.observable("aaaa");
+         
         // Operations
         self.calc = function () {
-            console.log('calc');
-            var input = new InputViewModel({ qty: self.qty._latestValue });
+            console.log('calc   ' + ko.toJSON(self)); 
             $.ajax("/Knockout/Input/Calc", {
-                data: ko.toJSON(  input ),
+                data: ko.toJSON(self),
                 type: "post", contentType: "application/json",
                 success: function (allData) {
                     console.log('success');
